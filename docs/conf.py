7dbdedbf90de77521b83a5e76e5ee60ad78e9fa9
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2026-09-02 22:58:56 -0700

    reverting a few changes

--- docs/conf.py
+++ docs/conf.py
@@ -106,7 +106,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "biocutils"
-copyright = "2026, Jayaram Kancherla"
+copyright = "2023, Aaron Lun"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -274,7 +274,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ("index", "user_guide.tex", "biocutils Documentation", "Jayaram Kancherla", "manual")
+    ("index", "user_guide.tex", "biocutils Documentation", "Aaron Lun", "manual")
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
